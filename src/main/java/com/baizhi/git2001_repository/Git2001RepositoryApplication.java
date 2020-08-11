e881d0aa9c9b2bdfd588edaa41a259dafb8b049d
zhangcn <15236674712@163.com>
2020-08-11 10:31:27 +0800

    git测试项目的基础环境

---
+++ src/main/java/com/baizhi/git2001_repository/Git2001RepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.git2001_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2001RepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2001RepositoryApplication.class, args);
+    }
+
+}
