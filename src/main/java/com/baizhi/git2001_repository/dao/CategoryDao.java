61721488c42c413d52cb473340c43c5f3ba3d66c
zhangcn <15236674712@163.com>
2020-08-11 11:07:36 +0800

    开发者B：添加了类别的实体类和dao接口

---
+++ src/main/java/com/baizhi/git2001_repository/dao/CategoryDao.java
@@ -0,0 +1,11 @@
+package com.baizhi.git2001_repository.dao;
+
+import com.baizhi.git2001_repository.entity.Category;
+
+import java.util.List;
+
+public interface CategoryDao {
+
+    List<Category> queryAll();
+
+}
